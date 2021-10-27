fffd37b472963d695356945730e6dc94c622ed10
darKleaf <2670583526@qq.com>
2021-10-27 15:44:24 +0800

    微调

--- testDate.js
+++ testDate.js
@@ -94,9 +94,86 @@ const res = {
 		"price": 133,
 		"describe": "一本书的描述",
 		"type": "军事",
-		"imageSmall":[
+		"imageSmall": [
 			"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d"
 		],
-		"detail":"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d"
+		"detail": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d"
+	}
+}
+
+
+// /book/queryList
+
+const res = {
+	"code": 200,
+	"message": "232323",
+	"result": {
+		"total": 20,
+		"data": [
+			{
+				"bookid": 5,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "艺术"
+			},
+			{
+				"bookid": 6,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "艺术"
+			},
+			{
+				"bookid": 7,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "艺术"
+			},
+			{
+				"bookid": 8,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "艺术"
+			},
+			{
+				"bookid": 1,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "军事"
+			},
+			{
+				"bookid": 2,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "军事"
+			},
+			{
+				"bookid": 3,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "军事"
+			},
+			{
+				"bookid": 4,
+				"name": "一本书",
+				"image": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage31.bookschina.com%2F2009%2F20091207%2Fs693406.jpg&refer=http%3A%2F%2Fimage31.bookschina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637891950&t=ce848c834012df4aea6052377076252d",
+				"price": 133,
+				"describe": "一本书的描述",
+				"type": "军事"
+			}
+		]
 	}
 }
